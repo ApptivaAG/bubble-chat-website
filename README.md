e3ef3a0b892ff661e96d4970c6de30627c3a1f95
Linus Hüsler <linus.huesler@apptiva.ch>
2021-10-26 18:27:07 +0200

    replace feature images

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Bubble Chat Website
+# Bubble Chat Webseite
+
+## Entwicklung
+
+- `npm i` zum Installieren aller Dependencies
+- `npm run dev` startet die Umgebung
+
+### Post CSS
+
+[PostCSS Language Support](https://marketplace.visualstudio.com/items?itemName=csstools.postcss) Erweiterung installieren
+
+## Deployment & Betrieb
+
+Durchs puschen deiner Änderungen wird die Seite automatisch gebuilet und auf Netlify deployed.
+
+## Printscreens
+
+Printscreens (z.B. vom CMS) sind in der Grösse 1500 x 900 Pixel erstellt.
+
+Du kannst in Chrome einen Printscreen der Webseite erstellen, indem du die Developer-Konsole geöffnet hast und dann `command + shift + P` klickst. Wählte danach `Screenshot erstellen` aus.
--- shortcodes/browserScreenshot.js
+++ shortcodes/browserScreenshot.js
@@ -1,7 +1,12 @@
 const imageShortcode = require("./image.js");
 
-// usage: {% browserScreenshot "./img/cat.jpg", "container-class"%}
-module.exports = async function browserScreenshot(src, alt, cls = "") {
+// usage: {% browserScreenshot "./img/cat.jpg", "Meine Katze", "container-class", "(min-width: 1248px) 600px, (min-width: 1024px) 40vw, 100vw"%}
+module.exports = async function browserScreenshot(
+  src,
+  alt,
+  cls = "",
+  sizes = "100vw"
+) {
   return `
     <div class="browserScreenshot ${cls}">
       <img class="browserScreenshot-browser" src="/img/browser-nav.svg" />
@@ -9,7 +14,7 @@ module.exports = async function browserScreenshot(src, alt, cls = "") {
         src,
         "browserScreenshot-img",
         alt,
-        "100vw",
+        sizes,
         [750, 1080, 1366, 1920]
       )} 
     </div>
