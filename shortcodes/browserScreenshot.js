12bda77940f3b66bfb8258b29c40a5a242ea08aa
Linus Hüsler <linus.huesler@apptiva.ch>
2021-10-27 09:53:25 +0200

    adjust browser screenshot

--- shortcodes/browserScreenshot.js
+++ shortcodes/browserScreenshot.js
@@ -9,7 +9,17 @@ module.exports = async function browserScreenshot(
 ) {
   return `
     <div class="browserScreenshot ${cls}">
-      <img class="browserScreenshot-browser" src="/img/browser-nav.svg" />
+      <div class="browserScreenshot-title-bar">
+        <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="50" fill="#ff4f4e"/>
+        </svg>
+        <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="50" fill="#ffbd00"/>
+        </svg>
+        <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="50" fill="#00cf1d"/>
+        </svg>
+      </div>
       ${await imageShortcode(
         src,
         "browserScreenshot-img",
